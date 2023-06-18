2b6d7ad196090025317e0d77f7db45bbd864a214
Jefferson Tales Canalli <jefferson.canalli@hotmail.com>
2023-06-18 09:48:32 -0300

    inclusão da camada web api

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,90 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase {
+
+        #region Atributos
+        private readonly ICategoryService _categoryService;
+        #endregion
+
+        #region Construtor
+        public CategoriesController(ICategoryService categoryService) {
+            this._categoryService = categoryService;
+        }
+        #endregion
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get() {
+
+            var categories = await this._categoryService.GetCategoriesAsync();
+
+            if (categories == null) {
+                return this.NotFound("Categories not found");
+            }
+            return this.Ok(categories);
+        }
+
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id) {
+
+            var category = await this._categoryService.GetByIdAsync(id);
+
+            if (category == null) {
+                return this.NotFound("Category not found");
+            }
+            return this.Ok(category);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO) {
+
+            if (categoryDTO == null) return this.BadRequest("Data invalid");
+
+            await this._categoryService.AddAsync(categoryDTO);
+
+            return new CreatedAtRouteResult(routeName: "GetCategory",
+                                            routeValues: new { id = categoryDTO.Id },
+                                            value: categoryDTO);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id,
+                                            [FromBody] CategoryDTO categoryDTO) {
+
+            if (id != categoryDTO.Id) return BadRequest();
+
+            if (categoryDTO == null) return BadRequest();
+
+            await this._categoryService.UpdateAsync(categoryDTO);
+
+            return this.Ok(categoryDTO);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id) {
+
+            var category = await this._categoryService.GetByIdAsync(id);
+
+            if (category == null) return this.NotFound("Category not found");
+
+            await this._categoryService.RemoveAsync(id);
+
+            return this.Ok(category);
+        }
+
+    }
+
+}
---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase {
+
+        #region Atributos
+        private readonly IProductService _productService;
+        #endregion
+
+        #region Construtor
+        public ProductsController(IProductService productService) {
+            this._productService = productService;
+        }
+        #endregion
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get() {
+
+            var products = await this._productService.GetProductsAsync();
+
+            if (products == null) {
+                return this.NotFound("Products not found");
+            }
+            return this.Ok(products);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id) {
+
+            var product = await this._productService.GetByIdAsync(id);
+
+            if (product == null) {
+                return this.NotFound("Product not found");
+            }
+            return this.Ok(product);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDTO) {
+
+            if (productDTO == null) return this.BadRequest("Data invalid");
+
+            await this._productService.AddAsync(productDTO);
+
+            return new CreatedAtRouteResult(routeName: "GetProduct",
+                                            routeValues: new { id = productDTO.Id },
+                                            value: productDTO);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id,
+                                            [FromBody] ProductDTO productDTO) {
+
+            if (id != productDTO.Id) return BadRequest("Data invalid");
+
+            if (productDTO == null) return BadRequest("Data invalid");
+
+            await this._productService.UpdateAsync(productDTO);
+
+            return this.Ok(productDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id) {
+
+            var productDTO = await this._productService.GetByIdAsync(id);
+
+            if (productDTO == null) return this.NotFound("Product not found");
+
+            await this._productService.RemoveAsync(id);
+
+            return this.Ok(productDTO);
+        }
+
+    }
+}
